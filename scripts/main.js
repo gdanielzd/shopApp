53a98faa8a3ff92edfe00be0fed116061b58137c
Gustavo Zorrilla <gdanielzd@gmail.com>
2024-04-08 17:37:29 -0400

    agregando aulgunas funciones y estilos

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const inputFielEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFielEl.value
+
+    console.log(inputValue)
+})
