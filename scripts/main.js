0c457e4ee1f286d375701819aa18df1175c04e21
Gustavo Zorrilla <gdanielzd@gmail.com>
2024-04-08 18:41:47 -0400

    conectando con la base de datos y probando con una lista de peliculas

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+/*import { initializeApp } from "../scrhttps://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const aapSettings ={
+    databaserURL: "https://playground-4d4f1-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+
+console.log(app)
+
 const inputFielEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
@@ -5,4 +15,25 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFielEl.value
 
     console.log(inputValue)
+}) */
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-4d4f1-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(moviesInDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
 })
